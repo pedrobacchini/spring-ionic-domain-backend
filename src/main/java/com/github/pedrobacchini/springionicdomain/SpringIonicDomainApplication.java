4f911b60d0058e28e866046af649ff9d3c668173
Pedro Bacchini <pedrobacchini@outlook.com>
2018-12-05 20:57:31 -0200

    Projeto Criado

---
+++ src/main/java/com/github/pedrobacchini/springionicdomain/SpringIonicDomainApplication.java
@@ -0,0 +1,12 @@
+package com.github.pedrobacchini.springionicdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIonicDomainApplication.class, args);
+    }
+}
