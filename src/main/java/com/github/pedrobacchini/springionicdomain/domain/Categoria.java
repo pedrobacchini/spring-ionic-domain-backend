5aeba0c5956024941cf18821d3d19383973ee9f9
Pedro Bacchini <pedrobacchini@outlook.com>
2018-12-08 08:33:18 -0200

    Adição do banco de dados h2 Mapeando a primeira classe domain para jpa

--- src/main/java/com/github/pedrobacchini/springionicdomain/domain/Categoria.java
+++ src/main/java/com/github/pedrobacchini/springionicdomain/domain/Categoria.java
@@ -1,12 +1,19 @@
 package com.github.pedrobacchini.springionicdomain.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = -4422320111986670655L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
