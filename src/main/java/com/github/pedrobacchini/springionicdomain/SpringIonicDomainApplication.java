a66050bcc13be2e82c469a1c983b0c880ff89797
Pedro Bacchini <pedrobacchini@outlook.com>
2018-12-11 19:33:17 -0200

    Criando operação de instanciação.

--- src/main/java/com/github/pedrobacchini/springionicdomain/SpringIonicDomainApplication.java
+++ src/main/java/com/github/pedrobacchini/springionicdomain/SpringIonicDomainApplication.java
@@ -1,12 +1,30 @@
 package com.github.pedrobacchini.springionicdomain;
 
+import com.github.pedrobacchini.springionicdomain.domain.Categoria;
+import com.github.pedrobacchini.springionicdomain.repository.CategoriaRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringIonicDomainApplication {
+public class SpringIonicDomainApplication implements CommandLineRunner {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public SpringIonicDomainApplication(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringIonicDomainApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escrítorio");
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
     }
 }
